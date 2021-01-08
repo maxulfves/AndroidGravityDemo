070c8fe23f8b9fcf21510e7f5def5e7ac5058c84
Max Ulfves <max.ulfves@gmail.com>
2021-01-08 20:09:25 +0200

    Initial commit

---
+++ app/src/main/java/WreckingBall.kt
@@ -0,0 +1,13 @@
+
+package com.example.gravitydemo
+
+class WreckingBall {
+
+    val position = Vector2D(0f, 0f);
+    val velocity = Vector2D(0.0f, 0f);
+
+    val r = 0.3f
+    val mass = 200.0f
+
+
+}
